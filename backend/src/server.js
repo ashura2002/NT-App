51ff353f02d490accf0a361afda7545a91bb830d
Jmark Dayna <jemarcdayna352@gmail.com>
2025-08-22 12:57:27 +0800

    try allow cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,13 +13,19 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173", // local dev
-      process.env.ALLOW_ORIGIN, // deployed frontend
-    ],
+    origin: true, // allow all origins temporarily for testing
     credentials: true,
   })
 );
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173", // local dev
+//       process.env.ALLOW_ORIGIN, // deployed frontend
+//     ],
+//     credentials: true,
+//   })
+// );
 
 app.use("/auth", userRouter);
 app.use("/api/notes", noteRoute);
